f5b6af653ad4498cc1cb2bbf49525f1fcbdc1728
Pamela <pamsilva.trabalho@hotmail.com>
2025-03-26 17:42:59 -0300

    Small adjustments

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -9,21 +9,23 @@ public class Menu {
         int opcao;
 
         while(true){
-            System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND +"--------------------------------------------------");
+            System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND +
+            "---------------------------------------------------");
             System.out.println("|                                                 |");
             System.out.println("|             BANCO DO BRAZIL COM Z               |");
             System.out.println("---------------------------------------------------");
             System.out.println("|                                                 |");
-            System.out.println("|           1 - Criar Conta                       |");            
+            System.out.println("|           1 - Criar Conta                       |");
             System.out.println("|           2 - Listar todas as Contas            |");
-            System.out.println("|           3 - Buscar Conta por Numero           |");            
+            System.out.println("|           3 - Buscar Conta por Numero           |");
             System.out.println("|           4 - Atualizar Dados da Conta          |");
-            System.out.println("|           5 - Apagar Conta                      |");            
+            System.out.println("|           5 - Apagar Conta                      |");
             System.out.println("|           6 - Sacar                             |");
-            System.out.println("|           7 - Depositar                         |");            
+            System.out.println("|           7 - Depositar                         |");
             System.out.println("|           8 - Transferir valores entre Contas   |");
-            System.out.println("|           9 - Sair                              |");            
-            System.out.println("|                                                 |");          
+            System.out.println("|           9 - Sair                              |");
+            System.out.println("|                                                 |");
+            
             System.out.println("---------------------------------------------------");
             System.out.println("|             Entre com a opção desejada:         |");
             System.out.println("|                                                 |" + Cores.TEXT_RESET);
@@ -79,11 +81,11 @@ public class Menu {
     }
 
     public static void sobre(){
-        System.out.println("--------------------------------------------------");
+        System.out.println("---------------------------------------------------");
         System.out.println("|  Projeto Desenvolvido por:                      |");
         System.out.println("|  Pamela Silva  -  pamsilva.trabalho@hotmail.com |");
         System.out.println("|  Github:  github.com/heypamela                  |");
         System.out.println("|  Linkedin: linkedin.com/in/pamelaoliveira14/    |");
-         System.out.println("---------------------------------------------------");
+        System.out.println("---------------------------------------------------");
     }
 }
