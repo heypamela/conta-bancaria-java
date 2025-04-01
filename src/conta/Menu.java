f9d2e6ade1ad445d27c2029e93917e1161a478f2
Pamela <pamsilva.trabalho@hotmail.com>
2025-04-01 11:17:10 -0300

    Restructuring menu class, adding keyPress method and exceptions

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,5 +1,6 @@
 package conta;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 import conta.model.ContaCorrente;
@@ -7,9 +8,11 @@ import conta.model.ContaPoupanca;
 import conta.util.Cores;
 
 public class Menu {
+    public static Scanner leia = new Scanner(System.in);
+    
     public static void main(String[] args) {
-        Scanner leia = new Scanner(System.in);
-
+        int opcao = 0;
+        
         //Teste da Classe Conta Corrente
         ContaCorrente cc1 = new ContaCorrente(2, 123, 1, "Mariana", 15000.0f, 1000.0f);
         cc1.visualizar();
@@ -17,7 +20,7 @@ public class Menu {
         cc1.visualizar();
         cc1.depositar(5000.0f);
         cc1.visualizar();
-
+        
         //Teste da Classe Conta Poupanca
         ContaPoupanca cp1 = new ContaPoupanca(3, 123, 2, "Victor", 100000.0f, 15);
         cp1.visualizar();
@@ -25,8 +28,6 @@ public class Menu {
         cp1.visualizar();
         cp1.depositar(5000.0f);
         cp1.visualizar();
-
-        int opcao;
 
         while(true){
             System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND +
@@ -50,7 +51,13 @@ public class Menu {
             System.out.println("|             Entre com a opção desejada:         |");
             System.out.println("|                                                 |" + Cores.TEXT_RESET);
 
-            opcao = leia.nextInt();
+            try {
+                opcao = leia.nextInt();
+            } catch (Exception e) {
+                System.out.println("\nDigite valores inteiros!");
+                leia.nextLine();
+                opcao = 0;
+            }
 
             if (opcao == 9) {
                 System.out.println(Cores.TEXT_WHITE_BOLD + 
@@ -63,38 +70,56 @@ public class Menu {
             switch (opcao) {
                 case 1:
                     System.out.println(Cores.TEXT_WHITE_BOLD +"Criar conta\n\n");
+
+                    keyPress();
                     break;
                 
                 case 2:
                     System.out.println(Cores.TEXT_WHITE_BOLD +"Listar todas as Contas\n\n");
+                    
+                    keyPress();
                     break;
                 
                 case 3:
                     System.out.println(Cores.TEXT_WHITE_BOLD +"Consultar dados da Conta - por número\n\n");
+                    
+                    keyPress();
                     break;
             
                 case 4:
                     System.out.println(Cores.TEXT_WHITE_BOLD +"Atualizar dados da Conta\n\n");
+                    
+                    keyPress();
                     break;
                 
                 case 5:
                     System.out.println(Cores.TEXT_WHITE_BOLD +"Apagar a Conta\n\n");
+                    
+                    keyPress();
                     break;
 
                 case 6:
                     System.out.println(Cores.TEXT_WHITE_BOLD +"Saque\n\n");
+                    
+                    keyPress();
                     break;
                 
                 case 7:
                     System.out.println(Cores.TEXT_WHITE_BOLD +"Depósito\n\n");
+                    
+                    keyPress();
                     break;
                 
                 case 8:
                     System.out.println(Cores.TEXT_WHITE_BOLD +"Transferência entre Contas\n\n");
+                    
+                    keyPress();
                     break;
 
                 default:
                     System.out.println(Cores.TEXT_RED_BOLD +"\nOpção Inválida!\n");
+                    
+                    keyPress();
                     break;
             }
         }
@@ -107,5 +132,14 @@ public class Menu {
         System.out.println("|  Github:  github.com/heypamela                  |");
         System.out.println("|  Linkedin: linkedin.com/in/pamelaoliveira14/    |");
         System.out.println("---------------------------------------------------");
+    }
+
+    public static void keyPress(){
+        try {
+            System.out.println(Cores.TEXT_RESET + "\n\nPressione Enter para Continuar...");
+            System.in.read();
+        } catch (IOException e) {
+            System.out.println("Você pressionou uma tecla diferente de enter!");
+        }
     }
 }
